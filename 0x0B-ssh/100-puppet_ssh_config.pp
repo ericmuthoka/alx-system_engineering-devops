2b97da899905428e88c1b6d877929cf6d9e47bcd
ericmuthoka <muthokaerick@gmail.com>
2023-09-24 05:53:26 -0700

    ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,17 @@
-#!/usr/bin/env bash
-# This script sets up ssh configuration using puppet
+#!/usr/bin/env puppet
+# Puppet manifest to configure SSH client
 
-exec {
+file { '/home/ubuntu/.ssh':
+  ensure  => directory,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0700',
+}
+
+file { '/home/ubuntu/.ssh/config':
+  ensure  => file,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  content => "IdentityFile ~/.ssh/school\nPasswordAuthentication no\n",
+  mode    => '0600',
+}
